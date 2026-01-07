7afac386195452cfe7ed76af575984cc3b943b40
Natouille04 <nathan.gouveia2008@gmail.com>
2026-01-07 14:17:04 +0100

    Refactorized AnalyticsCounter to multiple methods

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,48 +1,55 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import com.hemebiotech.analytics.ISymptomReader;
+import com.hemebiotech.analytics.ISymptomWriter;
+
+import java.util.*;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+	private static ISymptomReader reader;
+	private static ISymptomWriter writer;
 
-		int i = 0;
-		int headCount = 0;
-
-		while (line != null) {
-			i++;
-
-			System.out.println("symptom from file: " + line);
-
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+		AnalyticsCounter.reader = reader;
+		AnalyticsCounter.writer = writer;
 	}
+
+	public List<String> getSymptoms() {
+        return reader.GetSymptoms();
+	}
+
+	public Map<String, Integer> countSymptoms(List<String> symptoms) {
+		Map<String, Integer> result = new HashMap<>();
+
+		for(String symptom : symptoms) {
+			if(result.get(symptom) == null) {
+				result.put(symptom, 1);
+			}
+
+			else {
+                result.compute(symptom, (k, valueIndex) -> valueIndex + 1);
+			}
+		}
+
+		return result;
+	}
+
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+		List<String> symptomsByKey = new ArrayList<>(symptoms.keySet());
+		Collections.sort(symptomsByKey);
+
+		Map<String, Integer> response = new LinkedHashMap<>();
+
+		for(String key : symptomsByKey) {
+			response.put(key, symptoms.get(key));
+		}
+
+		return response;
+	}
+
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		writer.writeSymptoms(symptoms);
+	}
+
+	public static void main(String[] args) throws Exception {}
 }
